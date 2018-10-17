aa9900f6e405c9f4fc593c0b83de6bbe82bf630f
bdurb <brian@rpmgreenville.com>
2018-10-17 14:57:58 -0700

    set up deck.go to return a full deck of 52 cards

--- deck.go
+++ deck.go
@@ -7,6 +7,23 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+// receiver argument by convention is 1 or 2 letters
+// i.e. in the func below (d deck)
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
